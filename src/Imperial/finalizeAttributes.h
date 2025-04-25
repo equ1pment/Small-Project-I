6100e339da856d3d175929760243a1d8e510ec13
Hóa Thạch Không Rao Bán <huynhtuankietkiet1@gmail.com>
2025-04-25 13:43:15 +0700

    finalizeAttributes.h

--- src/Imperial/finalizeAttributes.h
+++ src/Imperial/finalizeAttributes.h
@@ -18,14 +18,12 @@ finalStats calculateFinalStats(const charData& character, const weapData& weapon
 
     float hpScale = 0.0f, atkScale = 0.0f, defScale = 0.0f, crScale = 0.0f, cdScale = 0.0f;
 
-    // Thuộc tính từ vũ khí
     if (weapon.attribute1.name == "HP") hpScale += weapon.attribute1.scale;
     else if (weapon.attribute1.name == "ATK") atkScale += weapon.attribute1.scale;
     else if (weapon.attribute1.name == "DEF") defScale += weapon.attribute1.scale;
     else if (weapon.attribute1.name == "CR") crScale += weapon.attribute1.scale;
     else if (weapon.attribute1.name == "CD") cdScale += weapon.attribute1.scale;
 
-    // Thuộc tính từ artifact
     vector<attrData> artiAttrs = { artifact.attribute1, artifact.attribute2, artifact.attribute3 };
     for (const auto& attr : artiAttrs) {
         if (attr.name == "HP") hpScale += attr.scale;
@@ -35,7 +33,6 @@ finalStats calculateFinalStats(const charData& character, const weapData& weapon
         else if (attr.name == "CD") cdScale += attr.scale;
     }
 
-    // Tính toán
     stats.HP = character.baseHP * (1 + hpScale);
     stats.ATK = (weapon.baseATK + character.baseATK) * (1 + atkScale);
     stats.DEF = character.baseDEF * (1 + defScale);
